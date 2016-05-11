26bc45d1731a601a6017815ada34db3d4b182865
xichu15 <xichu15@gmail.com>
2016-05-11 13:54:18 +0200

    moj commit dla Dudsona!!!

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,13 @@
  */
 package test;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author NP550P5C-SO4PL
@@ -15,7 +22,18 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            String text = "admin";
+            md.update(text.getBytes("UTF-8")); // Change this to "UTF-16" if needed
+            byte[] digest = md.digest();
+            BigInteger bigInt = new BigInteger(1, digest);
+            String output = bigInt.toString(16);
+
+            System.out.println(output);
+
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            
+        }
     }
-    
 }
